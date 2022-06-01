603013174e7313d6dd4180d11fa883317383db6e
hp10 <102642072+hp10@users.noreply.github.com>
2022-05-31 21:49:23 -0400

    Add files via upload
    The downloader for all the files.

---
+++ Download.bat
@@ -0,0 +1,12 @@
+@echo off
+md C:Users/%username%/XMathFiles
+cd C:Users/%username%/XMathFiles
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/Terminal.bat -Outfile Terminal.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/XMathA.bat -Outfile XMathA.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/XMathD.bat -Outfile XMathD.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/XMathDoubles.bat -Outfile XMathDoubles.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/XMathM.bat -Outfile XMathM.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/XMathS.bat -Outfile XMathS.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/rungetq.bat -Outfile rungetq.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/hp10/XMath/main/datetest.temp -Outfile datetest.temp"
+exit
